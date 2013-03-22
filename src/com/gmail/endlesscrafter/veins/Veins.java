3bb827154b1a0508d079fc34e0b78405cc190d12
Endlesscrafter <endlesscrafter@gmail.com>
2013-03-22 09:30:52 +0100

    Create Veins-Main

---
+++ src/com/gmail/endlesscrafter/veins/Veins.java
@@ -0,0 +1,21 @@
+package com.gmail.endlesscrafter.veins;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Veins extends JavaPlugin {
+
+	@Override
+	public void onDisable() {
+		
+		super.onDisable();
+	}
+
+	@Override
+	public void onEnable() {
+		
+		super.onEnable();
+	}
+	
+	
+
+}
